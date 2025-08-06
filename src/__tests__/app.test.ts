b3e15179ae79594ca962984fced650ed85e49b64
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-06 15:55:10 +0900

    add test for healthcheck

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,6 +1,14 @@
 import request from 'supertest';
 import app from '../app';
 
+describe('GET / for healthcheck', () => {
+  it('should return a OK', async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("OK");
+  })
+})
+
 describe('GET /api/v1/', () => {
   it('should return a hello message', async () => {
     const res = await request(app).get('/api/v1/');
