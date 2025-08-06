5378676ddbda3ef8119abaecedacb76e6544e49a
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-06 14:50:04 +0900

    add healthcheck

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ app.use(express.json());
 // Routes
 app.use("/api/v1/", itemRoutes);
 
+// Health check
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // Global error handler
 app.use(errorHandler);
 
