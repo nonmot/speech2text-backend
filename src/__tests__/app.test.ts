24a100cd21c21823be3dc30a08c70db7dd46aff6
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-12 00:25:28 +0900

    success test

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -4,7 +4,7 @@ import app from '../app';
 describe('GET / for healthcheck', () => {
   it('should return a OK', async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toBe(400);
+    expect(res.statusCode).toBe(200);
     expect(res.text).toBe("OK");
   })
 })
