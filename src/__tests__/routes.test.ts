08a701ecd0ca75041df6a2282929388f933e7097
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-11 21:07:34 +0900

    rewrite test for green (should be modified)

--- src/__tests__/routes.test.ts
+++ src/__tests__/routes.test.ts
@@ -2,8 +2,8 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Routes', () => {
-  it('GET /api/v1/models should return 200', async () => {
-    const res = await request(app).get('/api/v1/models');
-    expect(res.status).toBe(200);
+  it('GET /api/v1/ should return 404', async () => {
+    const res = await request(app).get('/api/v1/');
+    expect(res.status).toBe(404);
   });
 })
